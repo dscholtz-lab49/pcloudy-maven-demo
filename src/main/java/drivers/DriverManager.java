0d37afb2215bfdbba1005e1cf5f46abcee22b041
dscholtz-lab49 <daniel.scholtz@iongroup.com>
2024-05-31 11:24:47 +0200

    Updated java version to latest

--- src/main/java/drivers/DriverManager.java
+++ src/main/java/drivers/DriverManager.java
@@ -2,27 +2,18 @@ package drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.GeckoDriverService;
-import org.openqa.selenium.remote.service.DriverFinder;
-
-import java.io.File;
-import java.nio.file.Path;
 
 public class DriverManager {
 
     private WebDriver driver;
-    protected File driverPath;
-    protected File browserPath;
 
     public WebDriver createAndroidDriver(String browserType) {
         switch (browserType.toLowerCase()) {
-            case "chrome":
+            case "chrome" -> {
                 ChromeOptions chromeOptions = new ChromeOptions();
-
                 chromeOptions.setBrowserVersion("stable");
                 chromeOptions.addArguments("--window-size=1920,1080");
                 chromeOptions.addArguments("--headless");
@@ -31,19 +22,15 @@ public class DriverManager {
                 chromeOptions.addArguments("--no-sandbox");
                 chromeOptions.addArguments("--ignore-certificate-errors");
                 chromeOptions.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
-
                 driver = new ChromeDriver(chromeOptions);
-                break;
-            case "firefox":
+            }
+            case "firefox" -> {
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
-
                 firefoxOptions.addArguments("-headless");
                 firefoxOptions.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
-
                 driver = new FirefoxDriver(firefoxOptions);
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid browser type: " + browserType);
+            }
+            default -> throw new IllegalArgumentException("Invalid browser type: " + browserType);
         }
         return driver;
     }
